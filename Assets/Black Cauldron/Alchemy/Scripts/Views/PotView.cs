dfbee2a8fa05db0eee9eaafa715001ef53cf6496
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2024-10-30 23:59:28 +0300

    Add pot

---
+++ Assets/Black Cauldron/Alchemy/Scripts/Views/PotView.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Ginox.BlackCauldron.Alchemy.Views
+{
+    public class PotView : MonoBehaviour
+    {
+        private const string START_FLOW = "OnStartFlow";
+        private const string STOP_FLOW = "OnStopFlow";
+        private const float MAX_ANGLE_FOR_CONTAIN_WATER = 30f;
+
+        [SerializeField]
+        private GameObject waterSurface;
+        [SerializeField]
+        private VisualEffect waterFlowVFX;
+
+        private bool isFilled;
+
+        public bool IsFilled { 
+            get => isFilled;
+            set
+            {
+                isFilled = value;
+                waterSurface.SetActive(value);
+            }
+        }
+
+        private void Awake()
+        {
+            waterSurface.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if (!IsFilled)
+                return;
+
+            var angle = 180 - Vector3.Angle(transform.up, Vector3.down);
+
+            if (angle >= MAX_ANGLE_FOR_CONTAIN_WATER)
+                Drain();
+            else
+                waterFlowVFX.SendEvent(STOP_FLOW);
+        }
+
+        public void Fill()
+        {
+            IsFilled = true;
+        }
+
+        public void Drain()
+        {
+            IsFilled = false;
+            waterFlowVFX.SendEvent(START_FLOW);
+        }
+    }
+}
---
+++ Assets/Black Cauldron/Alchemy/Scripts/Views/WaterBarrelView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Ginox.BlackCauldron.Alchemy.Views
+{
+    public class WaterBarrelView : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<PotView>(out var pot))
+                return;
+
+            if (pot.IsFilled)
+                return;
+
+            pot.Fill();
+        }
+    }
+}
