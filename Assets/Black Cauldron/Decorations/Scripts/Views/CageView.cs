7c3e95370161e794deb4e5ccbcc7b9003a906423
Marat Zaripov <49365339+GinoxXP@users.noreply.github.com>
2025-02-01 15:42:46 +0300

    Add rabbit trash bin

---
+++ Assets/Black Cauldron/Decorations/Scripts/Views/CageView.cs
@@ -0,0 +1,28 @@
+﻿using Ginox.BlackCauldron.Alchemy.Views;
+using System.Linq;
+using UnityEngine;
+
+namespace Ginox.BlackCauldron.Decorations.Views
+{
+    public class CageView : MonoBehaviour
+    {
+        private static readonly string CRASH_TRIGGER = "IsCrash";
+
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var ingredient = other.GetComponentsInParent<MonoBehaviour>().OfType<AIngredientView>().FirstOrDefault();
+            if (ingredient != null)
+            {
+                Destroy(ingredient.gameObject);
+                animator.SetTrigger(CRASH_TRIGGER);
+            }
+        }
+    }
+}
